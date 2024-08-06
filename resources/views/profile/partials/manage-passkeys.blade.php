1d3d6dbceed0147181526ccd363a11226972bc40
Luis Parrado <luisprmat@gmail.com>
2024-08-06 16:10:47 -0500

    Add Passkey model and setup DB

--- resources/views/profile/partials/manage-passkeys.blade.php
+++ resources/views/profile/partials/manage-passkeys.blade.php
@@ -38,26 +38,31 @@
             {{ __('Your Passkeys') }}
         </h3>
         <ul class="mt-2">
-            <li class="flex items-center justify-between px-2 py-2">
-                <div class="flex flex-col">
-                    <span class="font-semibold dark:text-gray-100">
-                        1Password
-                    </span>
-                    <span
-                        class="text-sm font-thin text-gray-600 dark:text-gray-400"
-                    >
-                        Added 2 weeks ago
-                    </span>
-                </div>
+            @foreach ($user->passkeys as $passkey)
+                <li class="flex items-center justify-between px-2 py-2">
+                    <div class="flex flex-col">
+                        <span class="font-semibold dark:text-gray-100">
+                            {{ $passkey->name }}
+                        </span>
+                        <span
+                            class="text-sm font-thin text-gray-600 dark:text-gray-400"
+                        >
+                            {{ __('Added') }}
+                            {{ $passkey->created_at->diffForHumans() }}
+                        </span>
+                    </div>
 
-                <form method="post" action="/">
-                    @csrf
-                    @method('DELETE')
+                    <form method="post" action="/">
+                        @csrf
+                        @method('DELETE')
 
-                    <input type="hidden" name="id" value="" />
-                    <x-danger-button>{{ __('Remove') }}</x-danger-button>
-                </form>
-            </li>
+                        <input type="hidden" name="id" value="" />
+                        <x-danger-button>
+                            {{ __('Remove') }}
+                        </x-danger-button>
+                    </form>
+                </li>
+            @endforeach
         </ul>
     </div>
 </section>
