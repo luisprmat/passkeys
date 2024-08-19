61ab7ed02c062ff27974d71acb6ac4fbe9773644
Luis Parrado <luisprmat@gmail.com>
2024-08-19 00:50:04 -0500

    Install Webauthn Lib and setup from backend - register options

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\PasskeyController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/passkeys/register', [PasskeyController::class, 'registerOptions'])
+    ->middleware('auth:sanctum');
